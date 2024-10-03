32b25c4fd9ed258d1ff0c7724d52904dac37dd0e
Cameron Donald <122597702+Cameron-Dongal@users.noreply.github.com>
2024-10-03 11:53:54 -0700

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,4 @@
+print("My name is Cameron Donald and my preffered pronouns are he/him.")
+print("My favorite movie is The Goonies, where a group of kids hunt for pirate treasure in order to save their neighborhood from being turned into a golf course.")
+print("My favorite food is chocolate ice cream because it tastes good.")
+
