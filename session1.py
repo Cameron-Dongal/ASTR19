c4ae28e5c7f16af8ce30768bac61470d9dc417a1
Cameron Donald <122597702+Cameron-Dongal@users.noreply.github.com>
2024-10-03 11:57:53 -0700

    Update session1.py

--- session1.py
+++ session1.py
@@ -1,4 +1,9 @@
-print("My name is Cameron Donald and my preffered pronouns are he/him.")
-print("My favorite movie is The Goonies, where a group of kids hunt for pirate treasure in order to save their neighborhood from being turned into a golf course.")
-print("My favorite food is chocolate ice cream because it tastes good.")
 
+def main():
+
+    print("My name is Cameron Donald and my preffered pronouns are he/him.")
+    print("My favorite movie is The Goonies, where a group of kids hunt for pirate treasure in order to save their neighborhood from being turned into a golf course.")
+    print("My favorite food is chocolate ice cream because it tastes good.")
+
+if __name__ == "__main__":
+    main()
