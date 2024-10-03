316a912a167e69f5596bb44be2b7ac9cb03f390b
Cameron Donald <122597702+Cameron-Dongal@users.noreply.github.com>
2024-10-03 12:11:11 -0700

    Add files via upload

---
+++ session3.py
@@ -0,0 +1,13 @@
+def function(x):
+    return x **3 + 8
+
+def main():
+    ans = function(9)
+    print(ans)
+    if ans > 27:
+        print("YAY!")
+        
+if __name__ == "__main__":
+    main()
+    
+    
