391dbb38d102464a366b6c8e5077e251bbb18860
Cameron Donald <122597702+Cameron-Dongal@users.noreply.github.com>
2024-10-03 12:03:41 -0700

    Add files via upload

---
+++ session2.py
@@ -0,0 +1,17 @@
+def main():
+    fl1 = 1.0
+    fl2 = 2.0
+    int1 = 1
+    int2 = 2
+    
+    sum = fl1 + fl2
+    print(sum)
+    
+    diff = int1 - int2
+    print(diff)
+    
+    prod = fl1 * int2
+    print(prod)
+    
+if __name__ == "__main__":
+    main()
