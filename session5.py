1ee89bd8ea66e65a72e58158bcc2eafbc040e211
Cameron Donald <122597702+Cameron-Dongal@users.noreply.github.com>
2024-10-15 07:45:22 -0700

    Add files via upload

---
+++ session5.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    x = np.linspace(0, 2, 1000)
+    y = np.sin(x)
+
+    plt.plot(x, y, label="sin(x)")
+    plt.xlabel("x")
+    plt.ylabel("sin(x)")
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
