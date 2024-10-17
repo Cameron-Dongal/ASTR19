b945704017906705168dd1646df80a2546d79443
Cameron Donald <122597702+Cameron-Dongal@users.noreply.github.com>
2024-10-17 12:19:44 -0700

    Add files via upload

--- session5.py
+++ session5.py
@@ -5,11 +5,17 @@ def main():
     x = np.linspace(0, 2*np.pi, 1000)
     y = np.sin(x)
 
+    for i in range(1000):
+        print(x[i], y[i])
+    
     plt.plot(x, y, label="sin(x)")
     plt.xlabel("x")
     plt.ylabel("sin(x)")
 
     plt.show()
+    
+    for i in range(1000):
+        print(x[i], y[i])
 
 if __name__ == "__main__":
     main()
